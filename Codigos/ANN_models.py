12b838ffcc764850b5bb1505747348e9701a76e4
Yerko <108831113+Yerko47@users.noreply.github.com>
2024-07-29 23:21:59 -0400

    ANN_models.py

---
+++ Codigos/ANN_models.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+
+class ANN_1(nn.Module):
+    def __init__(self, input_size, drop):
+        super(ANN_1, self).__init__()
+        self.fc_layers = nn.ModuleList([
+            nn.Linear(input_size,160),
+            nn.Linear(160,320),
+            nn.Linear(320,160),
+            nn.Linear(160,80),
+            nn.Linear(80,20),
+            nn.Linear(20,5),
+            nn.Linear(5,1)
+        ])        
+
+        self.drop_layers = nn.ModuleList([
+            nn.Dropout(drop),
+            nn.Dropout(drop),
+            nn.Dropout(drop),
+            nn.Dropout(drop)
+        ])
+
+        self.bn_layers = nn.ModuleList([
+            nn.BatchNorm1d(160),
+            nn.BatchNorm1d(320),
+            nn.BatchNorm1d(160),
+            nn.BatchNorm1d(80),
+            nn.BatchNorm1d(20),
+            nn.BatchNorm1d(5),
+        ])
+
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        for i, (fc, bn) in enumerate(zip(self.fc_layers[:-1], self.bn_layers)):
+            x = fc(x)
+            x = bn(x)
+            x = self.activation(x)
+            if i < len(self.drop_layers):
+                x = self.drop_layers[i](x)
+
+        x = self.fc_layers[-1](x) 
+        return x
+
+class ANN_2(nn.Module):
+    def __init__(self, input_size, drop):
+        super(ANN_2, self).__init__()
+        self.fc_layers = nn.ModuleList([
+            nn.Linear(input_size,320),
+            nn.Linear(320,160),
+            nn.Linear(160,160),
+            nn.Linear(160,80),
+            nn.Linear(80,20),
+            nn.Linear(20,5),
+            nn.Linear(5,1)
+        ])        
+
+        self.drop_layers = nn.ModuleList([
+            nn.Dropout(drop),
+            nn.Dropout(drop),
+            nn.Dropout(drop),
+            nn.Dropout(drop),
+            nn.Dropout(drop)
+        ])
+
+        self.bn_layers = nn.ModuleList([
+            nn.BatchNorm1d(320),
+            nn.BatchNorm1d(160),
+            nn.BatchNorm1d(160),
+            nn.BatchNorm1d(80),
+            nn.BatchNorm1d(20),
+            nn.BatchNorm1d(5),
+        ])
+
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        for i, (fc, bn) in enumerate(zip(self.fc_layers[:-1], self.bn_layers)):
+            x = fc(x)
+            x = bn(x)
+            x = self.activation(x)
+            if i < len(self.drop_layers):
+                x = self.drop_layers[i](x)
+
+        x = self.fc_layers[-1](x) 
+        return x
