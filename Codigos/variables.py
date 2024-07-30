97b5679d84a2058e8f53efbeface0652e9c0382e
Yerko <108831113+Yerko47@users.noreply.github.com>
2024-07-29 23:19:34 -0400

    variables.py

--- Codigos/variables.py
+++ Codigos/variables.py
@@ -1,15 +1,13 @@
-""" Variable list """
-
-####### [ Date ] #######
+###### [ Data ] ######
 in_year = 1995
 out_year = 2018
 
 
-####### [ Geomagnétic Auroral Index ] #######
-auroral_index = 'AE_INDEX'
+###### [ Geomagnetic Auroral Index ] ######
+auroral_index = 'AE_INDEX'      # AE_INDEX    |    AL_INDEX    |    AU_INDEX
 
 
-####### [ Traget ] #######
+###### [ Parameters ] ######
 omni_param = ['B_Total',
               'BY_GSM',
               'BZ_GSM',
@@ -20,37 +18,66 @@ omni_param = ['B_Total',
               'T',
               'Pressure',
               'E_Field',
-              'flow_speed'
               ]
 
-auroral_param = ['AU_INDEX',
-                 'AE_INDEX',
-                 'AL_INDEX']
+auroral_param = ['AE_INDEX', 
+                 'AL_INDEX', 
+                 'AU_INDEX'] 
 
 
-####### [ Neural Network Model Parameters ] #######
-type_model = 'ANN'
-scaler = 'robust'
-shifty = 3
+###### [ Split Train/Val/Test Set  ] ######
+set_split = 'organized'         # organized => TimeSerieSplit()   |   random => train_test_split()    |   list => storm_list.csv
 
-num_epoch = 300
-batch_train_val = 2080
-barch_test = 1040
-learning_rate = 1e-5
-drop = 0.2
-early_stop = 100
+## [ set_split = organized ] ##
+n_split_train_val_test = 4
+n_split_train_val = 3
 
-####### LSTM #######
+## [ set_split = random ] ##
+test_size = 0.2
+val_size = 0.2
+
+
+###### [ Neural Networks Parameters ] ######
+type_model = 'ANN'              # ANN   |   LSTM    |   CNN    |    etc
+type_neural_network = 2         # 1     |   2   |   3.... (eg: type_model = ANN and type_neural_network = 1  ==>   model = ANN_1)
+scaler = 'robust'               # robust => RobustScaler    |    standard => StandardScaler
+shift_length = 5
+num_epoch = 100
+
+## [ Loader ] ##
+batch_train = 1040
+batch_val = 1040
+batch_test = 520
+
+## [ Hyperparameters ] ##
+learning_rate = 1e-4
+weight_decay = 1e-2
+scheduler_option = False
+patience = 10
+drop = 0.4
+
+
+## [ ANN ] ##
+
+
+## [ LSTM ] ##
 num_layer = 3
 
-####### CNN #######
-kernel = 3
-padding = 1
-
-####### RNN #######
+## [ CNN ] ##
 
 
-####### [ File ] #######
+## [ RNN ] ##
+
+
+###### [ Plots and correlation ] ######
+correlation = 'spearman'        # spearman    |    kendall
+processPLOT = False
+
+###### [ Load ] ######
+processOMNI = True
+
+
+###### [ File ] ######
 omni_file = f'/data/omni/hro_1min/'
 desktop_file = f'/home/yerko/Desktop/'
 save_raw_file = desktop_file + f'raw_data/'
@@ -60,23 +87,4 @@ result_file = processing_file + f'result/'
 plot_file = desktop_file + f'plots/'
 
 save_feather = save_raw_file + f'omni_data_{in_year}_to_{out_year}.feather'
-
-
-####### [ Train/Val/Test Set ] #######
-storm_list = False
-n_split_train_val_test = 4
-n_split_train_val = 3
-
-
-####### [ Plot ] #######
-correlation = 'spearman'
-processPLOT = False
-
-
-####### [ Download ] #######
-processOMNI = True
-
-
-
-
 
